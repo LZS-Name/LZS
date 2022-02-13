30c4138d2dfa33e78a30d671df0a4d8193e1583a
Yong Ming <yongming1124@gmail.com>
2022-02-13 13:04:03 +0800

    added approve and reject application api

---
+++ backend/src/constant/application.constant.ts
@@ -0,0 +1,12 @@
+const ApplicationConstant = {
+
+    //status
+    status:{
+        PENDING_FIRST_APPROVAL: "PENDING_FIRST_APPROVAL",
+        PENDING_SECOND_APPROVAL: "PENDING_SECOND_APPROVAL",
+        APPROVED: "APPROVED",
+        REJECTED: "REJECTED"
+    }
+}
+
+export default ApplicationConstant;
