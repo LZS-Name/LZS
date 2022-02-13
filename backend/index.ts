338d5e57ff63f27f97c1f894091f1b683f6d9042
He Lin <helinchooi@gmail.com>
2022-02-13 11:59:47 +0800

    Change backend port to 3001

--- backend/index.ts
+++ backend/index.ts
@@ -1,27 +1,29 @@
-require('dotenv').config()
+require("dotenv").config();
 import express, { Application } from "express";
-const mongoose = require('mongoose');
-const apiRouter = require('./src/controllers');
+const mongoose = require("mongoose");
+const apiRouter = require("./src/controllers");
 
 const app: Application = express();
-const port = 3000;
+const port = 3001;
 
 // Database Connection
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error: Error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
-db.on('disconnected', ()=> console.log('Mongoose connection to DB disconnected'))
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error: Error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+db.on("disconnected", () =>
+  console.log("Mongoose connection to DB disconnected")
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/', apiRouter)
+app.use("/api/", apiRouter);
 
 try {
-    app.listen(port, (): void => {
-        console.log(`Connected successfully on port ${port}`);
-    });
+  app.listen(port, (): void => {
+    console.log(`Connected successfully on port ${port}`);
+  });
 } catch (error: any) {
-    console.error(`Error occured: ${error.message}`);
+  console.error(`Error occured: ${error.message}`);
 }
