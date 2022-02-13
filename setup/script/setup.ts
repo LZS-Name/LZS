d427dbade67c41bd46873e9b73cdd0d467f6a47e
Yong Ming <yongming1124@gmail.com>
2022-02-13 15:33:58 +0800

    added setup db script

---
+++ setup/script/setup.ts
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+import { Application } from "../../backend/src/models/Application";
+import application_mock from '../mock_data/application.mock.json';
+const mongoose = require("mongoose");
+
+// Database Connection
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error: Error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+db.on("disconnected", () =>
+  console.log("Mongoose connection to DB disconnected")
+);
+
+//Application
+Application.insertMany(application_mock).then(()=>{
+    mongoose.disconnect();
+})
+
